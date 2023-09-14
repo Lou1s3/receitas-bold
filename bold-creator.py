f93aec2f41b9d2cdba4522d8bd6675553923cec7
Emmanuelle Louise <112348634+Lou1s3@users.noreply.github.com>
2023-09-14 11:04:19 -0300

    Create bold-creator.py

---
+++ bold-creator.py
@@ -0,0 +1,9 @@
+flavours = ["cookie n' cream", "cholocate truffe", "banoffe"]
+
+print("Choose your BOLD:")
+for flavour in flavours:
+  print(flavour)
+
+user_input = input("What bold do you want?")
+
+print("DELICIOUS")
